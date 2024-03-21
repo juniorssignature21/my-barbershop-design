a884be2de6ac6fd65dbd6c0794cee411b209e1e0
Junior's Signature <chibuzor.john.2018@gmail.com>
2024-03-21 13:01:11 +0100

    added onclick with js

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-function styleDisplay() {
-    document.getElementById("Home").style.backgroundColor="red";
+function styledisplay() {
+    document.getElementById("home").style.color="#fff48e";
+    document.getElementById("abouts").style.color="#fff";
+    document.getElementById("service").style.color="#fff";
+    document.getElementById("teams").style.color="#fff";
+    document.getElementById("review").style.color="#fff";
+    document.getElementById("books").style.color="#fff";
+}
+function aboutdisplay() {
+      document.getElementById("abouts").style.color="#fff48e";
+      document.getElementById("home").style.color="#fff";
+      document.getElementById("service").style.color="#fff";
+      document.getElementById("teams").style.color="#fff";
+      document.getElementById("review").style.color="#fff";
+      document.getElementById("books").style.color="#fff";
+}
+function servicedisplay() {
+    document.getElementById("service").style.color="#fff48e";
+    document.getElementById("home").style.color="#fff";
+    document.getElementById("abouts").style.color="#fff";
+    document.getElementById("teams").style.color="#fff";
+    document.getElementById("review").style.color="#fff";
+    document.getElementById("books").style.color="#fff";
+}
+function teamsdisplay() {
+    document.getElementById("teams").style.color="#fff48e";
+    document.getElementById("home").style.color="#fff";
+    document.getElementById("abouts").style.color="#fff";
+    document.getElementById("service").style.color="#fff";
+    document.getElementById("review").style.color="#fff";
+    document.getElementById("books").style.color="#fff";
+}
+function reviewdisplay() {
+    document.getElementById("review").style.color="#fff48e";
+    document.getElementById("home").style.color="#fff";
+    document.getElementById("abouts").style.color="#fff";
+    document.getElementById("service").style.color="#fff";
+    document.getElementById("teams").style.color="#fff";
+    document.getElementById("books").style.color="#fff";
+}
+function bookdisplay() {
+    document.getElementById("books").style.color="#fff48e";
+    document.getElementById("home").style.color="#fff";
+    document.getElementById("abouts").style.color="#fff";
+    document.getElementById("service").style.color="#fff";
+    document.getElementById("teams").style.color="#fff";
+    document.getElementById("review").style.color="#fff";
+}
+matchMedia("(max-width: 995px)").matches;
+function menubtn() {
+    document.getElementById("myDiv").style.display="flex"
+    document.getElementById("menu").style.display="none"
+    document.getElementById("cancel").style.display="flex"
+
+}
+function cancelbtn() {
+    document.getElementById("myDiv").style.display="none"
+    document.getElementById("menu").style.display="flex"
+    document.getElementById("cancel").style.display="none"
 }
